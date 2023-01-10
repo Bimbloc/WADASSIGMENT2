3ee7b42872f270f08543856a2913097a2593db52
Rocio Sanchez <rocisa08@ucm.es>
2023-01-10 12:14:13 +0100

    man it compiles but at  what cost

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import SiteHeader from './components/siteHeader'
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; 
 import UpcomingPage from "./pages/upcomingMoviesPage";
